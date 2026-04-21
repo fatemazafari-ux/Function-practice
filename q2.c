037a33b28b93269cb8518ec5da435e4219fd9f43
fatemazafari-ux <fatema.zafari@auw.edu.bd>
2026-04-21 23:08:09 +0600

    Add files via upload

---
+++ q2.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int add(int a, int b) {
+    return a + b;
+}
+
+int main() {
+    int x, y, result;
+
+    scanf("%d %d", &x, &y);
+
+    result = add(x, y);
+
+    printf("Sum = %d", result);
+
+    return 0;
+}
